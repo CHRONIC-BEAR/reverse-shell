fdc60bc5201ceefcb2bd90f6487f7735036527bc
CHRONIC-BEAR <120120756+CHRONIC-BEAR@users.noreply.github.com>
2022-12-30 12:06:35 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,15 +1,16 @@
-#client.py >> will later turn this into an exe file for Windows
-#This program will be put on the client system, that when executed, sends data to my server that I can see
+#This program will be put on the client system, that when executed, takes data received from the server, and prints it to the screen
 import socket
 
-SERVER_IP = '192.168.1.79'
-SERVER_PORT = 5678
+SERVER_IP = '' #enter IP of the listening device (server)
+SERVER_PORT = 5678 #can be any port that is not being used..
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((SERVER_IP, SERVER_PORT)) #self explanatory I think...
-    #once connected, the first thing that happens is the server sends the message
+    s.connect((SERVER_IP, SERVER_PORT)) #the connect function takes the server IP and the port the server is listening on as arguments
+    #once connected, the server sends a message
+    
     data = s.recv(1024) #s.recv is the function that will receive that message, we can store it in a variable called data
-    #the argument s.recv() takes is the buffer_size, how many bytes need to come in before it starts processing/executing all the code below it
+    #the argument s.recv() takes is the buffer_size, how many bytes need to come in before it starts processing/executing the code below it
+    
     print(data) #print the message to the screen
 
-input() #Just so the message will stay on the screen instead of closing out
+input() #so the message will stay on the screen instead of closing out
